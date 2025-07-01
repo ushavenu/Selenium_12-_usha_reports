8fd03f7195ee6c03803f79fac63d5a4d364f6564
VENUGOBAL <VENUGOBAL@LAPTOP-H4MMLA92>
2025-07-01 20:36:24 +0530

    new demo testcase added

---
+++ demoprojectgithub/src/test/java/demoprojectgithub/demo.java
@@ -0,0 +1,15 @@
+package demoprojectgithub;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void launchBrowser()
+	{
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://demowebshop.tricentis.com/");
+	}
+}
--- demowebshop1.1/src/test/java/books/new_Test.java
+++
@@ -1,9 +0,0 @@
-package books;
-
-public class new_Test {
-@Test
-  public void demo()
-  {
-    reporter.log("running demo",true);
-  }
-}
